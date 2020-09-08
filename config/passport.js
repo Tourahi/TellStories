d9189723bc4a785582f54761e731742d389f8d01
maromaroXD <amine-fb-fb@hotmail.fr>
2020-09-08 09:02:34 +0100

    Setting up GoogleStrategy and auth routes then adding partials

--- config/passport.js
+++ config/passport.js
@@ -13,6 +13,23 @@ module.exports = function(passport) {
     // console.log(profile);
     const newUser = {
       googleId : profile.id,
+      displayName : profile.displayName,
+      firstName   : profile.name.familyName,
+      lastName   : profile.name.givenName,
+      image      : profile.photos[0].value,
+    }
+
+    try {
+      let user = await User.findOne({ googleId :  profile.id});
+      if (user) {
+        done(null , user);
+      }
+      else {
+        user = User.create(newUser);
+        done(null , user);
+      }
+    } catch (err) {
+      console.log(err);
     }
   }));
 
--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,14 @@ router.get('/google' , passport.authenticate('google' , { scope : ['profile']}))
 router.get('/google/callback' , passport.authenticate('google' , {
   failureRedirect : '/'
 }) , (req , res) => {
-  res.redirect('dashboard');
+  res.redirect('/dashboard');
+});
+
+// @desc  Logout user
+// @met/route GET /auth/logout
+router.get('/logout' , (req , res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 
